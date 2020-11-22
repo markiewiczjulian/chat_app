e140f75e6fd2a3da5d1817d9a456435901f5ef45
markiewicz.julian@gmail.com <markiewicz.julian@gmail.com>
2020-11-22 11:40:33 +0100

    added indentation in chat (changed formatter)

--- frontend/src/components/Chat.vue
+++ frontend/src/components/Chat.vue
@@ -20,107 +20,107 @@
 </template>
 
 <script>
-import io from "socket.io-client";
-import objectIdToTimestamp from "objectid-to-timestamp";
-import emoji from "node-emoji";
-import { VEmojiPicker } from "v-emoji-picker";
+  import io from "socket.io-client";
+  import objectIdToTimestamp from "objectid-to-timestamp";
+  import emoji from "node-emoji";
+  import { VEmojiPicker } from "v-emoji-picker";
 
-export default {
-  name: "Chat",
-  data() {
-    return {
-      socket: {},
-      messages: [],
-      message: "",
-      userTypingInfo: "",
-      emojiPanelOpen: false,
-    };
-  },
-  components: {
-    VEmojiPicker,
-  },
-  created() {
-    this.socket = io("http://localhost:3000/");
-  },
-  mounted() {
-    this.axios.get("http://localhost:3000/chatRoom").then((res) => {
-      res.data.map((el) => {
-        el.message = this.parseEmoji(el.message);
+  export default {
+    name: "Chat",
+    data() {
+      return {
+        socket: {},
+        messages: [],
+        message: "",
+        userTypingInfo: "",
+        emojiPanelOpen: false,
+      };
+    },
+    components: {
+      VEmojiPicker,
+    },
+    created() {
+      this.socket = io("http://localhost:3000/");
+    },
+    mounted() {
+      this.axios.get("http://localhost:3000/chatRoom").then((res) => {
+        res.data.map((el) => {
+          el.message = this.parseEmoji(el.message);
+        });
+        this.messages = res.data;
       });
-      this.messages = res.data;
-    });
 
-    this.socket.on("receivedMessage", (data) => {
-      data.message = this.parseEmoji(data.message);
-      this.messages.push(data);
-      this.userTypingInfo = "";
-    });
+      this.socket.on("receivedMessage", (data) => {
+        data.message = this.parseEmoji(data.message);
+        this.messages.push(data);
+        this.userTypingInfo = "";
+      });
 
-    this.socket.on("notifyStopTyping", () => {
-      console.log("STOPPED TYPING");
-      this.userTypingInfo = "";
-    });
+      this.socket.on("notifyStopTyping", () => {
+        console.log("STOPPED TYPING");
+        this.userTypingInfo = "";
+      });
 
-    this.socket.on("notifyTyping", (user) => {
-      console.log("TYPING");
-      this.userTypingInfo = `${user} is typing right now...`;
-    });
-  },
-  watch: {
-    message() {
-      const user = "USERPLACEHOLDER";
-      const curMessage = this.message;
-      this.message = this.parseEmoji(this.message);
-      console.log(curMessage, this.message);
-      console.log("QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ");
-      if (this.message.length) {
-        setTimeout(() => {
-          if (this.message === curMessage) {
-            this.socket.emit("stoppedTyping", user);
-          } else {
-            this.socket.emit("typing", user);
-          }
-        }, 500);
-      } else {
-        this.socket.emit("stoppedTyping", user);
-      }
+      this.socket.on("notifyTyping", (user) => {
+        console.log("TYPING");
+        this.userTypingInfo = `${user} is typing right now...`;
+      });
     },
-  },
-  methods: {
-    toggleEmojiPanel() {
-      this.emojiPanelOpen = !this.emojiPanelOpen;
+    watch: {
+      message() {
+        const user = "USERPLACEHOLDER";
+        const curMessage = this.message;
+        this.message = this.parseEmoji(this.message);
+        console.log(curMessage, this.message);
+        console.log("QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ");
+        if (this.message.length) {
+          setTimeout(() => {
+            if (this.message === curMessage) {
+              this.socket.emit("stoppedTyping", user);
+            } else {
+              this.socket.emit("typing", user);
+            }
+          }, 500);
+        } else {
+          this.socket.emit("stoppedTyping", user);
+        }
+      },
     },
-    onMissing(emojiName) {
-      return emojiName;
+    methods: {
+      toggleEmojiPanel() {
+        this.emojiPanelOpen = !this.emojiPanelOpen;
+      },
+      onMissing(emojiName) {
+        return emojiName;
+      },
+      parseEmoji(txt) {
+        return emoji.emojify(txt, this.onMissing);
+      },
+      unparseEmoji(txt) {
+        return emoji.unemojify(txt);
+      },
+      addNewMsg(msg) {
+        this.socket.emit("addedMessage", this.unparseEmoji(msg));
+        this.message = "";
+        this.emojiPanelOpen = false;
+      },
+      idToTimestamp(id) {
+        //   toLocaleString powinno zadziałać z pudełka bez żadnego łątania stringami
+        const date = new Date(objectIdToTimestamp(id));
+        return `${date.getDate()}-${date.getMonth() +
+          1}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
+      },
+      selectEmoji(emoji) {
+        this.message = this.message + emoji.data;
+        this.emojiPanelOpen = false;
+      },
     },
-    parseEmoji(txt) {
-      return emoji.emojify(txt, this.onMissing);
+    computed: {
+      btnDisabled() {
+        return !this.message;
+      },
     },
-    unparseEmoji(txt) {
-      return emoji.unemojify(txt);
-    },
-    addNewMsg(msg) {
-      this.socket.emit("addedMessage", this.unparseEmoji(msg));
-      this.message = "";
-      this.emojiPanelOpen = false;
-    },
-    idToTimestamp(id) {
-      //   toLocaleString powinno zadziałać z pudełka bez żadnego łątania stringami
-      const date = new Date(objectIdToTimestamp(id));
-      return `${date.getDate()}-${date.getMonth() +
-        1}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
-    },
-    selectEmoji(emoji) {
-      this.message = this.message + emoji.data;
-      this.emojiPanelOpen = false;
-    },
-  },
-  computed: {
-    btnDisabled() {
-      return !this.message;
-    },
-  },
-};
+  };
 </script>
 
 <style lang="scss">
