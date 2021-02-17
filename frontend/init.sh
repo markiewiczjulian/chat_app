559dadea1229bfdae38144c3be63ffc6c9e27bb6
markiewicz.julian@gmail.com <markiewicz.julian@gmail.com>
2021-02-17 15:03:25 +0100

    changes to formatting in scripts

--- frontend/init.sh
+++ frontend/init.sh
@@ -2,37 +2,37 @@
 set -e
 IFS='|'
 
-VUECONFIG=”{\
-\”SourceDir\”:\”src\”,\
-\”DistributionDir\”:\”dist\”,\
-\”BuildCommand\”:\”npm run-script build\”,\
-\”StartCommand\”:\”npm run-script start\”\
-}”
+VUECONFIG="{\
+\"SourceDir\":\"src\",\
+\"DistributionDir\":\"dist\",\
+\"BuildCommand\":\"npm run-script build\",\
+\"StartCommand\":\"npm run-script start\"\
+}"
 
-AWSCLOUDFORMATIONCONFIG=”{\
-\”configLevel\”:\”project\”,\
-\”useProfile\”:true,\
-\”profileName\”:\”default\”,\
+AWSCLOUDFORMATIONCONFIG="{\
+\"configLevel\":\"project\",\
+\"useProfile\":true,\
+\"profileName\":\"default\",\
 \"accessKeyId\":\"$AWS_ACCESS_KEY_ID\",\
 \"secretAccessKey\":\"$AWS_SECRET_ACCESS_KEY\",\
 \"region\":\"us-east-1\"\
-}”
+}"
 
-AMPLIFY=”{\
-\”projectName\”:\”chat-app\”,\
-\”envName\”:\”dev\”,\
-\”defaultEditor\”:\”code\”\
-}”
+AMPLIFY="{\
+\"projectName\":\"chat-app\",\
+\"envName\":\"dev\",\
+\"defaultEditor\":\"code\"\
+}"
 
-FRONTEND=”{\
-\”frontend\”:\”javascript\”,\
-\”framework\”:\”vue\”,\
-\”config\”:$VUECONFIG\
-}”
+FRONTEND="{\
+\"frontend\":\"javascript\",\
+\"framework\":\"vue\",\
+\"config\":$VUECONFIG\
+}"
 
-PROVIDERS=”{\
-\”awscloudformation\”:$AWSCLOUDFORMATIONCONFIG\
-}”
+PROVIDERS="{\
+\"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
+}"
 
 amplify init \
 —- amplify $AMPLIFY \
--- frontend/push.sh
+++ frontend/push.sh
@@ -2,13 +2,13 @@
 set -e
 IFS='|'
 
-CODEGEN=”{\
-\”generateCode\”:true,\
-\”codeLanguage\”:\”javascript\”,\
-\”fileNamePattern\”:\”src/graphql/**/*.js\”,\
-\”generatedFileName\”:\”API\”,\
-\”generateDocs\”:true\
-}”
+CODEGEN="{\
+\"generateCode\":true,\
+\"codeLanguage\":\"javascript\",\
+\"fileNamePattern\":\"src/graphql/**/*.js\",\
+\"generatedFileName\":\"API\",\
+\"generateDocs\":true\
+}"
 
 amplify push \
 --codegen $CODEGEN \
