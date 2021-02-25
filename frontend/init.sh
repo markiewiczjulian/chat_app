3a2c26f4056700ef65d99bab6cc1778a20c456e4
markiewicz.julian@gmail.com <markiewicz.julian@gmail.com>
2021-02-25 15:06:08 +0100

    changes to workflow and to script

--- frontend/init.sh
+++ frontend/init.sh
@@ -12,8 +12,8 @@ VUECONFIG="{\
 AWSCLOUDFORMATIONCONFIG="{\
 \"configLevel\":\"project\",\
 \"useProfile\":false,\
-\"accessKeyId\":\"${AWS_ACCESS_KEY_ID}\",\
-\"secretAccessKey\":\"${AWS_SECRET_ACCESS_KEY}\",\
+\"accessKeyId\":$AWS_ACCESS_KEY_ID,\
+\"secretAccessKey\":$AWS_SECRET_ACCESS_KEY,\
 \"region\":\"eu-west-1\"\
 }"
 
